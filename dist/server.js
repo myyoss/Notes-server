822b600aaf31bf363dcb9564ea0be132e16fdfb8
Yossi <asnafy.junior@gmail.com>
2022-12-25 18:43:11 +0200

    dist

--- dist/server.js
+++ dist/server.js
@@ -6,21 +6,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const noteRoute_1 = __importDefault(require("./routes/noteRoute"));
+const cors = require("cors");
 const app = (0, express_1.default)();
-require('dotenv').config();
-const port = process.env.PORT || 4005;
-app.use(express_1.default.static('public/build'));
 app.use(express_1.default.json());
+// {
+//     origin:'*',
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200,
+//  }
+app.use(cors({ origin: '*', }));
+require("dotenv").config();
+const port = process.env.PORT || 4005;
+// app.use(express.static('public/build'))
 const url = process.env.MONGODB_URI;
-mongoose_1.default.connect(url)
-    .then(res => {
+mongoose_1.default
+    .connect(url)
+    .then((res) => {
     console.log("connected to Mongoose");
 })
     .catch((err) => {
     console.log("Failed to connect to Mongoose:");
     console.log(err.message);
 });
-app.use('/api/notes', noteRoute_1.default);
+app.use("/api/notes", noteRoute_1.default);
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
