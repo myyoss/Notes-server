f85f2c2fd31008a646061a1cb9ef3cdc1afea3c7
Yossi <asnafy.junior@gmail.com>
2022-12-25 14:53:10 +0200

    cors

--- server.ts
+++ server.ts
@@ -4,6 +4,11 @@ import noteRouter from './routes/noteRoute';
 const cors = require("cors");
 
 const app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(cors());
 require('dotenv').config();
 
