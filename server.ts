22621625898a195a20fbea287c72856048d43f7b
Yossi <asnafy.junior@gmail.com>
2022-12-25 18:09:40 +0200

    *

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ const app = express();
 //     optionSuccessStatus:200,
 //  }
 app.use(cors(
-    {origin:'https://notes-client.onrender.com',}
+    {origin:'*',}
 ));
 require("dotenv").config();
 
