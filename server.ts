dec98c175425bb3eb9324cc9b15b39847b2de47f
Yossi <asnafy.junior@gmail.com>
2022-12-25 17:34:19 +0200

    delete cors()

--- server.ts
+++ server.ts
@@ -4,20 +4,13 @@ import noteRouter from "./routes/noteRoute";
 const cors = require("cors");
 
 const app = express();
-const corsOptions = {
-  origin: [
-    "http://localhost:4005",
-    "https://notes-server-uwt7.onrender.com",
-    "https://notes-server-uwt7.onrender.com",
-  ],
-  credentials: true,
-};
+
 // {
 //     origin:'*',
 //     credentials:true,            //access-control-allow-credentials:true
 //     optionSuccessStatus:200,
 //  }
-app.use(cors(corsOptions));
+app.use(cors());
 require("dotenv").config();
 
 const port = process.env.PORT || 4005;
