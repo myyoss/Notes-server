8f6a066b2dfd56dac69c7e108014ed8656eab948
Yossi <asnafy.junior@gmail.com>
2022-12-25 17:14:01 +0200

    cors

--- server.ts
+++ server.ts
@@ -1,16 +1,24 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import noteRouter from './routes/noteRoute';
+import express from "express";
+import mongoose from "mongoose";
+import noteRouter from "./routes/noteRoute";
 const cors = require("cors");
 
 const app = express();
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
+const corsOptions = {
+  origin: [
+    "http://localhost:4005",
+    "https://notes-server-uwt7.onrender.com",
+    "https://notes-server-uwt7.onrender.com",
+  ],
+  credentials: true,
+};
+// {
+//     origin:'*',
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200,
+//  }
 app.use(cors(corsOptions));
-require('dotenv').config();
+require("dotenv").config();
 
 const port = process.env.PORT || 4005;
 
@@ -19,19 +27,18 @@ app.use(express.json());
 
 const url = process.env.MONGODB_URI;
 
-mongoose.connect(url)
-    .then(res => {
-        console.log("connected to Mongoose");
-    })
-    .catch((err) => {
-        console.log("Failed to connect to Mongoose:")
-        console.log(err.message);
-    });
+mongoose
+  .connect(url)
+  .then((res) => {
+    console.log("connected to Mongoose");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to Mongoose:");
+    console.log(err.message);
+  });
 
-
-app.use('/api/notes', noteRouter);
-
+app.use("/api/notes", noteRouter);
 
 app.listen(port, () => {
-    return console.log(`Express is listening at http://localhost:${port}`);
+  return console.log(`Express is listening at http://localhost:${port}`);
 });
